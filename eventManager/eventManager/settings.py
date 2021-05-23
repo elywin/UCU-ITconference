e6b5bbbbc1d88a53b647be01a67f4535ae48a274
elywin <lukeeraelywin@gmail.com>
2021-05-23 11:29:50 +0300

    added whitenoisemiddleware

--- eventManager/eventManager/settings.py
+++ eventManager/eventManager/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'eventManager.urls'
@@ -119,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/files/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (

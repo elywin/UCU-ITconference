36642ff9ba2e18bed7c1278bb6e31d3e53dd7b69
elywin <lukeeraelywin@gmail.com>
2021-05-23 11:36:11 +0300

    added staticfiles directory

--- eventManager/eventManager/settings.py
+++ eventManager/eventManager/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'eventManager.urls'
@@ -120,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/files/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (

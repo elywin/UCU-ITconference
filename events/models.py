7f4ffc181f4dbfd2905bf37bb67a28ca214d4776
elywin <lukeeraelywin@gmail.com>
2021-06-12 15:57:41 +0300

    adjust description size in models

--- events/models.py
+++ events/models.py
@@ -19,7 +19,7 @@ class Guest(models.Model):
 
 class AboutUs(models.Model): 
     title = models.CharField(max_length=200, default='About Us') 
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=1000)
     
     def __str__(self):
         return self.title

0fe2c1666f4157d47e1ab4d996908c343be9010f
elywin <lukeeraelywin@gmail.com>
2021-06-29 02:34:16 +0300

    register model

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Guest, AboutUs, OurGoal
+from .models import Guest, AboutUs, OurGoal, FAQs
 
 
 # Register your models here.
 admin.site.register(Guest)
 admin.site.register(AboutUs)
 admin.site.register(OurGoal)
+admin.site.register(FAQs)
